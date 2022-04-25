a08832d59b5082be9d771dd745505aae0b8912c3
Rom BALAX <rbalax@yahoo.co.uk>
2022-04-25 16:47:08 +0900

    done on gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,17 +1,32 @@
-
-
-
 function activateGallery() {
-let thumbnails = document.querySelector("#gallery-thumbs").
-                          querySelectorAll("img");
-let mainImage = document.querySelector("#gallery-photo img");
+  let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+  let mainImage  = document.querySelector("#gallery-photo img");
+  let galleryInfo = document.querySelector("#gallery-info");
+  let title       = galleryInfo.querySelector(".title");
+  let description = galleryInfo.querySelector(".description");
 
-thumbnails.forEach(function(thumbnail) {
-  thumbnail.addEventListener("click", function() {
-    // code to set clicked image as main image
-    let newImageSrc = thumbnail.dataset.largeVersion;
-    mainImage.setAttribute("src", newImageSrc);
-    mainImage.setAttribute("alt", thumbnail.alt);
+  thumbnails.forEach(function(thumbnail) {
+    thumbnail.addEventListener("click", function() {
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      mainImage.setAttribute("src", newImageSrc);
+      document.querySelector(".current").classList.remove("current");
+      thumbnail.parentNode.classList.add("current");
+      title.innerHTML       = thumbnail.dataset.title;
+      description.innerHTML = thumbnail.dataset.description;
+    });
   });
-});
 }
+
+// function activateGallery() {
+//   let thumbnails = document.querySelector("#gallery-thumbs").
+//                             querySelectorAll("img");
+//   let mainImage = document.querySelector("#gallery-photo img");
+
+//   thumbnails.forEach(function(thumbnail) {
+//     thumbnail.addEventListener("click", function() {
+//       // Set clicked image as main image.
+//       let newImageSrc = thumbnail.dataset.largeVersion;
+//       mainImage.setAttribute("src", newImageSrc);
+//     });
+//   });
+// }
