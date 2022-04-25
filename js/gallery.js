1d0b407a27472acd971e8c1c3c3615cf6e191133
Rom BALAX <rbalax@yahoo.co.uk>
2022-04-25 18:06:08 +0900

    done  gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,32 +1,40 @@
 function activateGallery() {
   let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+
   let mainImage  = document.querySelector("#gallery-photo img");
-  let galleryInfo = document.querySelector("#gallery-info");
-  let title       = galleryInfo.querySelector(".title");
-  let description = galleryInfo.querySelector(".description");
+
+
 
   thumbnails.forEach(function(thumbnail) {
+
     thumbnail.addEventListener("click", function() {
-      let newImageSrc = thumbnail.dataset.largeVersion;
+
+      // Preload large images.
+    let newImageSrc  = thumbnail.dataset.largeVersion;
+    console.log(newImageSrc);
+    console.log(thumbnail.dataset.title);
+    let largeVersion = new Image();
+    console.log(largeVersion);
+    largeVersion.src = newImageSrc;
+    console.log(largeVersion.src);
+
+    //Set big display image
+
       mainImage.setAttribute("src", newImageSrc);
+
+    // Change which image is current
+
       document.querySelector(".current").classList.remove("current");
       thumbnail.parentNode.classList.add("current");
+
+    //update image info
+
+      let galleryInfo = document.querySelector("#gallery-info");
+      let title       = galleryInfo.querySelector(".title");
+      let description = galleryInfo.querySelector(".description");
+
       title.innerHTML       = thumbnail.dataset.title;
       description.innerHTML = thumbnail.dataset.description;
     });
   });
 }
-
-// function activateGallery() {
-//   let thumbnails = document.querySelector("#gallery-thumbs").
-//                             querySelectorAll("img");
-//   let mainImage = document.querySelector("#gallery-photo img");
-
-//   thumbnails.forEach(function(thumbnail) {
-//     thumbnail.addEventListener("click", function() {
-//       // Set clicked image as main image.
-//       let newImageSrc = thumbnail.dataset.largeVersion;
-//       mainImage.setAttribute("src", newImageSrc);
-//     });
-//   });
-// }
